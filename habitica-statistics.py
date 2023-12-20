163b37480df0d91e20a5b9f6e8112df30e472abe
Polina Guseva <polina_0901@mail.ru>
2023-12-20 17:44:57 +0100

    Add information input

--- habitica-statistics.py
+++ habitica-statistics.py
@@ -1,11 +1,27 @@
-import csv
+import csv, click
 from datetime import datetime, timedelta
 
 csv_file_path = 'habitica-tasks-history.csv'
 filtered_rows = []
 
-start_date = datetime.strptime('01.01.2023', '%d.%m.%Y')
-end_date = datetime.strptime('31.12.2023', '%d.%m.%Y')
+while True:
+    start_date_inp = input('Start date\n(Enter date as dd.mm.yyyy): ')
+
+    try:
+        start_date = datetime.strptime(start_date_inp, '%d.%m.%Y')
+        break
+    except ValueError:
+        print('Incorrect format, should be dd.mm.yyyy')
+
+while True:
+    end_date_inp = input('End date\n(Enter date as dd.mm.yyyy): ')
+
+    try:
+        end_date = datetime.strptime(end_date_inp, '%d.%m.%Y')
+        break
+    except ValueError:
+        print('Incorrect format, should be dd.mm.yyyy')
+
 number_of_days = (end_date - start_date).days
 
 def calculate_percentage(part, whole):
@@ -19,12 +35,32 @@ with open(csv_file_path, 'r', newline='', encoding='utf-8') as file:
     reader = list(csv.DictReader(file))
 
     if (not task_ids or len(task_ids) == 0):
-        task_ids = list(set(map(lambda row: row['Task ID'], reader)))
+        if click.confirm('Do you want to enter task ID?', default=True):
+            task_id_inp = input('Enter task ID: ')
+            task_ids.append(task_id_inp)
+
+            while True:
+                print('Your task IDs:')
+                for task_id in task_ids:
+                    print(f"{task_id}")
+                if click.confirm('Do you want to enter another one task ID?', default=True):
+                    task_id_inp = input('Enter task ID: ')
+                    task_ids.append(task_id_inp)
+                else:
+                    break
+
+        else:
+            task_ids = list(set(map(lambda row: row['Task ID'], reader)))
 
     streaks = {task_id: [[]] for task_id in task_ids}
 
     for task_id in task_ids:
-        task_rows = filter(lambda row: row['Task ID'] == task_id, reader)
+        task_rows = list(filter(lambda row: row['Task ID'] == task_id, reader))
+
+        if (not task_rows):
+            print(f'Task ID {task_id} not found')
+            continue
+
         sorted_rows = sorted(task_rows, key=lambda row: datetime.strptime(
             row['Date'], '%Y-%m-%d %H:%M:%S'))
 
@@ -71,11 +107,19 @@ with open(csv_file_path, 'r', newline='', encoding='utf-8') as file:
 
         longest_streak = max(streaks[task_id], key=lambda streak: len(streak))
         longest_streak_len = len(longest_streak)
-        longest_streak_first_date = longest_streak[0].strftime('%d.%m.%Y')
-        longest_streak_last_date = longest_streak[-1].strftime('%d.%m.%Y')
+
+        is_streak_exist = longest_streak_len > 0
+        is_streak_one_day = longest_streak_len == 1
+
+        longest_streak_first_date = longest_streak[0].strftime('%d.%m.%Y') if is_streak_exist else ''
+        longest_streak_last_date = longest_streak[-1].strftime('%d.%m.%Y') if is_streak_exist else ''
+
+        longest_streak_result = f"{longest_streak_len} days, {longest_streak_first_date} - {longest_streak_last_date}" \
+            if is_streak_exist and not is_streak_one_day \
+                else 'no multi-day streaks'
         
         print(f"{task_name}:\n \
             {len(task_days)} times \n \
             {percentage}% \n \
-            Longest streak: {longest_streak_len} days, {longest_streak_first_date} - {longest_streak_last_date} \n\n \
+            Longest streak: {longest_streak_result} \n\n \
         ")
